a6b520d1d52ab8df03c3a4134357a889d3fd58b3
tanjiro-ka <the.chaotic.dev@gmail.com>
2025-12-07 22:27:56 +0100

    feat(models): add Movie model and migration (Closes #17)

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,9 +1,15 @@
 from logging.config import fileConfig
+import sys
+import os
+from pathlib import Path
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+# Add /app to path so we can import 'app' package
+sys.path.insert(0, '/app')
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,9 +22,12 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from database import Base 
-from models.user import User
-target_metadata = Base.metadata
+# Import using app as package to make relative imports work
+import app.database
+import app.models.user
+import app.models.movie
+
+target_metadata = app.database.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ backend/app/models/movie.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, Text, Float
+from sqlalchemy.sql import func
+from ..database import Base
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+
+    ghibli_api_id = Column(String, unique=True, nullable=False, index=True)
+    title = Column(String, nullable=False, index=True)
+    original_title = Column(String, nullable=True)
+    original_title_romanised = Column(String, nullable=True, index=True)
+    description = Column(Text, nullable=True)
+    director = Column(String, nullable=True, index=True)
+    producer = Column(String, nullable=True, index=True)
+    release_date = Column(Integer, nullable=True, index=True)
+    rt_score = Column(Float, nullable=True, default=0.0, index=True)
+    image_url = Column(String, nullable=True)
+    movie_banner = Column(String, nullable=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
